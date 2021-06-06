425e172935559469c2088e7fb372e43e04b35bc2
Paul Thornley <pthornley1981@gmail.com>
2021-06-06 21:42:23 +0100

    File containing variable params (No personal passwords in here. All passwords are public knowledge and for testing purposes.

--- getDeviceInfo.py
+++ getDeviceInfo.py
@@ -5,9 +5,29 @@ import json
 import DNACconf
 import AuthToken
 
-token  = AuthToken.getAuthToken()
+token = AuthToken.getAuthToken()
 user = DNACconf.user
 password = DNACconf.password
 baseURL = DNACconf.devURL
+auth = DNACconf.auth
+
+payload = {}
+headers = {
+    'X-Auth-Token': token,
+    'Content-Type': 'application/json',
+    'Authorization': auth
+}
+
 
 def getDeviceList():
+    url = baseURL + "/dna/intent/api/v1/network-device"
+    response = requests.get(url, headers=headers, data=payload)
+    print(response.text)
+
+
+def getInterfaces():
+    url = baseURL + "/InterfaceURL" # Needs updating, check CDNAC docs
+    response = requests.get(url, headers=headers, data=payload)
+    print(response)
+
+
