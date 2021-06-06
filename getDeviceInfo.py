156ab4bcf56162f98e86a94582d1553fb542f939
Paul Thornley <pthornley1981@gmail.com>
2021-06-06 22:51:48 +0100

    Functions created to interact with Cisco DNA Center

--- getDeviceInfo.py
+++ getDeviceInfo.py
@@ -4,6 +4,7 @@ import sys
 import json
 import DNACconf
 import AuthToken
+import pprint
 
 token = AuthToken.getAuthToken()
 user = DNACconf.user
@@ -22,12 +23,12 @@ headers = {
 def getDeviceList():
     url = baseURL + "/dna/intent/api/v1/network-device"
     response = requests.get(url, headers=headers, data=payload)
-    output = response.json()
-    
+    output = response.json()['response']
+    return output
 
 # Get Interface details for device
 def getInterfaces(device):
     url = baseURL + "/InterfaceURL" # Needs updating, check CDNAC docs
     response = requests.get(url, headers=headers, data=payload)
-    print(response)
+
 
