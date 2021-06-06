1ebe4090d89e09af2143dcaab15997dedc235b7e
Paul Thornley <pthornley1981@gmail.com>
2021-06-06 21:29:28 +0100

    Script to gather authenitcation token from device

--- AuthToken.py
+++ AuthToken.py
@@ -5,7 +5,6 @@ import json
 
 urllib3.disable_warnings()
 baseURL = DNACconf.devURL
-url = baseURL + "/dna/system/api/v1/auth/token"
 payload = {}
 headers = {
     'Authorization': DNACconf.auth
@@ -13,6 +12,7 @@ headers = {
 
 
 def getAuthToken():
+    url = baseURL + "/dna/system/api/v1/auth/token"
     response = requests.post(url, headers=headers, data=payload)
     output = response.json()
     return output['Token']
